f27d858c9efa6efd2d797b60102484ea40d6a484
zhuchunshu <laravel@88.com>
2021-07-08 16:42:35 +0800

    基本配置

--- config/autoload/codefec.php
+++ config/autoload/codefec.php
@@ -2,6 +2,6 @@
 return [
     "app" => [
         "name" => env("CodeFec_App_Name", "CodeFec"),
-        "csrf" => (bool)env("CodeFec_App_Csrf", false)
+        "csrf" => (bool)env("CodeFec_App_Csrf", true)
     ]
 ];
