3087c3718e2fe434e058647883d9ab40470eb340
zhuchunshu <laravel@88.com>
2021-07-12 11:10:51 +0800

    更新:压缩js代码

--- public/js/admin/component.js
+++ public/js/admin/component.js
@@ -1,15 +1,1 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!*****************************************!*\
-  !*** ./resources/js/admin/component.js ***!
-  \*****************************************/
-var header_left = {
-  methods: {
-    reload: function reload() {
-      location.reload();
-    }
-  }
-};
-Vue.createApp(header_left).mount("#vue-header-left");
-/******/ })()
-;
+(()=>{var e={methods:{reload:function(){location.reload()}}};Vue.createApp(e).mount("#vue-header-left")})();
