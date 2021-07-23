297929a4dbad2ab71c2037b8ee8ff11a4afddd0a
zhuchunshu <laravel@88.com>
2021-07-23 22:58:30 +0800

    更新:新增页头模板

--- app/Plugins/Core/resources/views/setting/theme/header.blade.php
+++ app/Plugins/Core/resources/views/setting/theme/header.blade.php
@@ -3,8 +3,8 @@
         <x-csrf/>
         <div class="mb-3">
             <label class="form-label">页头风格</label>
-            <input type="number" min="1" max="4" class="form-control" v-model="data.core_theme_header">
-            <small>填写以下风格ID,1-4</small>
+            <input type="number" min="1" max="8" class="form-control" v-model="data.core_theme_header">
+            <small>填写以下风格ID,1-8</small>
         </div>
         <div class="row row-cards">
             <div class="col-md-12">
@@ -25,6 +25,26 @@
             <div class="col-md-12">
                 <a class="link link-accent card-title">风格4</a>
                 <iframe style="width: 100%" src="/help/core/viewRender/components.theme.header-4.html"></iframe>
+            </div>
+            <hr/>
+            <div class="col-md-12">
+                <a class="link link-accent card-title">风格5</a>
+                <iframe style="width: 100%" src="/help/core/viewRender/components.theme.header-5.html"></iframe>
+            </div>
+            <hr/>
+            <div class="col-md-12">
+                <a class="link link-accent card-title">风格6</a>
+                <iframe style="width: 100%" src="/help/core/viewRender/components.theme.header-6.html"></iframe>
+            </div>
+            <hr/>
+            <div class="col-md-12">
+                <a class="link link-accent card-title">风格7</a>
+                <iframe style="width: 100%" src="/help/core/viewRender/components.theme.header-7.html"></iframe>
+            </div>
+            <hr/>
+            <div class="col-md-12">
+                <a class="link link-accent card-title">风格8</a>
+                <iframe style="width: 100%" src="/help/core/viewRender/components.theme.header-8.html"></iframe>
             </div>
         </div>
     </div>
