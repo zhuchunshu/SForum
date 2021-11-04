2a715445baabbc3f786452d7070cdb29eb7c72d2
zhuchunshu <laravel@88.com>
2021-11-04 20:57:34 +0800

    更新:创建usersCollection模型

---
+++ app/Plugins/User/src/Models/UsersCollection.php
@@ -0,0 +1,38 @@
+<?php
+
+declare (strict_types=1);
+namespace App\Plugins\User\src\Models;
+
+use App\Model\Model;
+use Carbon\Carbon;
+
+/**
+ * @property int $id 
+ * @property string $title 
+ * @property string $content 
+ * @property string $action 
+ * @property string $user_id 
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ */
+class UsersCollection extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'users_collections';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['user_id','title','content','created_at','updated_at','action'];
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = ['id' => 'integer', 'created_at' => 'datetime', 'updated_at' => 'datetime'];
+}
