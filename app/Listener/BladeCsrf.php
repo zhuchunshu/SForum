1ca5166adc36d39db46ad32046901cac0eea3442
zhuchunshu <laravel@88.com>
2021-12-19 23:38:32 +0800

    更新:blade模板支持@csrf指令

---
+++ app/Listener/BladeCsrf.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Listener;
+
+use Hyperf\Event\Annotation\Listener;
+use Hyperf\Framework\Event\BootApplication;
+use Hyperf\ViewEngine\Blade;
+use Psr\Container\ContainerInterface;
+use Hyperf\Event\Contract\ListenerInterface;
+
+/**
+ * @Listener
+ */
+class BladeCsrf implements ListenerInterface
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    public function listen(): array
+    {
+        return [
+            BootApplication::class
+        ];
+    }
+
+    public function process(object $event)
+    {
+        Blade::directive('csrf', function () {
+            $token =csrf_token();
+            return "<?php echo <<<HTML
+<input type='hidden' name='_token' value='{$token}'>
+
+HTML;
+; ?>";
+        });
+    }
+}
