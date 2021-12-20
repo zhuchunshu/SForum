5ff27cc9f3c142c06a256cfee1b5c329d3e927b8
zhuchunshu <laravel@88.com>
2021-12-20 19:37:47 +0800

    更新

--- app/Plugins/Core/resources/views/layouts/themes/menu/menu-quanxian.blade.php
+++
@@ -1,9 +0,0 @@
-@if (!core_menu_pd($key))
-    @if(auth()->Class()['permission-value']>=$value['quanxian'])
-        @include('Core::layouts.themes.menu.single')
-    @endif
-@else
-    @if(auth()->Class()['permission-value']>=$value['quanxian'])
-        @include('Core::layouts.themes.menu.multiple')
-    @endif
-@endif
--- app/Plugins/Core/routes.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Hyperf\HttpServer\Router\Router;
-
-Router::addServer('websocket', function () {
-    Router::get('/core', \App\Plugins\Core\src\Controller\WebSocket\CoreController::class);
-});
