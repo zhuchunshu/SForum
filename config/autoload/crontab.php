821a21c7be38d03c0e8d6c0214d061425040c4d6
zhuchunshu <zhuchunshu999@gmail.com>
2022-02-16 18:17:53 +0800

    更新:支持定时任务

---
+++ config/autoload/crontab.php
@@ -0,0 +1,5 @@
+<?php
+return [
+	// 是否开启定时任务
+	'enable' => true,
+];
--- config/autoload/processes.php
+++ config/autoload/processes.php
@@ -11,4 +11,5 @@ declare(strict_types=1);
  */
 return [
     Hyperf\AsyncQueue\Process\ConsumerProcess::class,
+	Hyperf\Crontab\Process\CrontabDispatcherProcess::class,
 ];
