b6e666d0cd3e27ea318534ade2d0091877c96604
zhuchunshu <zhuchunshu999@gmail.com>
2022-02-27 13:40:47 +0800

    修复:解决图片预览插件的bug

--- public/js/app.js
+++ public/js/app.js
@@ -2492,8 +2492,8 @@ process.umask = function() { return 0; };
 /******/ 		var installedChunks = {
 /******/ 			"/js/app": 0,
 /******/ 			"plugins/Core/css/core": 0,
-/******/ 			"css/app": 0,
-/******/ 			"plugins/Topic/css/app": 0
+/******/ 			"plugins/Topic/css/app": 0,
+/******/ 			"css/app": 0
 /******/ 		};
 /******/ 		
 /******/ 		// no chunk on demand loading
@@ -2541,10 +2541,10 @@ process.umask = function() { return 0; };
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module depends on other loaded chunks and execution need to be delayed
-/******/ 	__webpack_require__.O(undefined, ["plugins/Core/css/core","css/app","plugins/Topic/css/app"], () => (__webpack_require__("./resources/js/app.js")))
-/******/ 	__webpack_require__.O(undefined, ["plugins/Core/css/core","css/app","plugins/Topic/css/app"], () => (__webpack_require__("./resources/sass/plugins/Topic/app.scss")))
-/******/ 	__webpack_require__.O(undefined, ["plugins/Core/css/core","css/app","plugins/Topic/css/app"], () => (__webpack_require__("./resources/sass/plugins/Core/core.scss")))
-/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["plugins/Core/css/core","css/app","plugins/Topic/css/app"], () => (__webpack_require__("./resources/sass/app.scss")))
+/******/ 	__webpack_require__.O(undefined, ["plugins/Core/css/core","plugins/Topic/css/app","css/app"], () => (__webpack_require__("./resources/js/app.js")))
+/******/ 	__webpack_require__.O(undefined, ["plugins/Core/css/core","plugins/Topic/css/app","css/app"], () => (__webpack_require__("./resources/sass/plugins/Topic/app.scss")))
+/******/ 	__webpack_require__.O(undefined, ["plugins/Core/css/core","plugins/Topic/css/app","css/app"], () => (__webpack_require__("./resources/sass/plugins/Core/core.scss")))
+/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, ["plugins/Core/css/core","plugins/Topic/css/app","css/app"], () => (__webpack_require__("./resources/sass/app.scss")))
 /******/ 	__webpack_exports__ = __webpack_require__.O(__webpack_exports__);
 /******/ 	
 /******/ })()
