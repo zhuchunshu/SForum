3e64ffd3e60b531c09d1e253ba30f5188eb27835
zhuchunshu <zhuchunshu999@gmail.com>
2022-03-09 16:05:26 +0800

    更新:优化缓存、重写后台设置、优化启动速度、重写登陆注册、支持用户名登陆、重写验证码、支持找回密码、优化邮箱验证、支持邀请注册

--- app/Plugins/Core/resources/package/js/sign.js
+++ app/Plugins/Core/resources/package/js/sign.js
@@ -5,12 +5,12 @@ if(document.getElementById("vue-core-sign-register")){
     const vcsr = {
         data() {
             return {
-                username:"",
-                email:"",
-                password:"",
-                cfpassword:"",
-                captcha:"",
-                endTime:CaptchaEndTime
+                username:null,
+                email:null,
+                password:null,
+                cfpassword:null,
+                captcha:null,
+                invitationCode:null,
             }
         },
         methods: {
@@ -22,73 +22,7 @@ if(document.getElementById("vue-core-sign-register")){
                     password:this.password,
                     cfpassword:this.cfpassword,
                     captcha:this.captcha,
-                    timer:""
-                })
-                    .then(response=>{
-                        var data = response.data;
-                        if(data.success===true){
-                            swal({
-                                icon:"success",
-                                title:data.result.msg
-                            })
-                            setTimeout(() => {
-                                location.href="/login"
-                            }, 1200);
-                        }else{
-                            if(data.result instanceof Array){
-                                var content = "";
-                                data.result.forEach(element => {
-                                    content = content+element+"\n"
-                                });
-                                swal({
-                                    icon : "error",
-                                    title: "出错啦!",
-                                    text:content
-                                });
-                            }else{
-                                swal({
-                                    icon:"error",
-                                    title:data.result.msg
-                                })
-                            }
-
-                        }
-                    })
-                    .catch(error=>{
-                        console.error(error)
-                        swal({
-                            title:"请求出错",
-                            icon:"error"
-                        })
-                    })
-            },
-            setTimeOut(){
-                this.endTime --;
-            }
-        },
-        mounted() {
-            this.timer = setInterval(this.setTimeOut, 1000);
-        }
-    }
-
-    Vue.createApp(vcsr).mount("#vue-core-sign-register")
-}
-
-
-if(document.getElementById("vue-core-sign-login")){
-    const vcsl = {
-        data(){
-            return {
-                email:"",
-                password:""
-            }
-        },
-        methods:{
-            submit(){
-                axios.post("/login",{
-                    _token:csrf_token,
-                    email:this.email,
-                    password:this.password,
+                    invitationCode:this.invitationCode
                 })
                     .then(response=>{
                         var data = response.data;
@@ -127,9 +61,299 @@ if(document.getElementById("vue-core-sign-login")){
                             icon:"error"
                         })
                     })
+            }
+        },
+        mounted() {
+
+        }
+    }
+
+    Vue.createApp(vcsr).mount("#vue-core-sign-register")
+}
+
+
+// 邮箱登陆
+if(document.getElementById("vue-core-sign-login")){
+    const vcsl = {
+        data(){
+            return {
+                email:null,
+                password:null,
+                captcha:null
+            }
+        },
+        methods:{
+            submit(){
+                axios.post("/login",{
+                    _token:csrf_token,
+                    email:this.email,
+                    password:this.password,
+                    captcha:this.captcha,
+                })
+                    .then(response=>{
+                        var data = response.data;
+                        if(data.success===true){
+                            swal({
+                                icon:"success",
+                                title:data.result.msg
+                            })
+                            setTimeout(()=>{
+                                location.href="/";
+                            },1000)
+
+                        }else{
+                            if(data.result instanceof Array){
+                                var content = "";
+                                data.result.forEach(element => {
+                                    content = content+element+"\n"
+                                });
+                                swal({
+                                    icon : "error",
+                                    title: "出错啦!",
+                                    text:content
+                                });
+                            }else{
+                                swal({
+                                    icon:"error",
+                                    title:data.result.msg
+                                })
+                            }
+
+                        }
+                    })
+                    .catch(error=>{
+                        console.error(error)
+                        swal({
+                            title:"请求出错",
+                            icon:"error"
+                        })
+                    })
             },
         }
     }
 
     Vue.createApp(vcsl).mount("#vue-core-sign-login")
+}
+
+// 邮箱登陆
+if(document.getElementById("vue-core-sign-login-username")){
+    const app = {
+        data(){
+            return {
+                username:null,
+                password:null,
+                captcha:null
+            }
+        },
+        methods:{
+            submit(){
+                axios.post("/login/username",{
+                    _token:csrf_token,
+                    username:this.username,
+                    password:this.password,
+                    captcha:this.captcha,
+                })
+                    .then(response=>{
+                        var data = response.data;
+                        if(data.success===true){
+                            swal({
+                                icon:"success",
+                                title:data.result.msg
+                            })
+                            setTimeout(()=>{
+                                location.href="/";
+                            },1000)
+
+                        }else{
+                            if(data.result instanceof Array){
+                                var content = "";
+                                data.result.forEach(element => {
+                                    content = content+element+"\n"
+                                });
+                                swal({
+                                    icon : "error",
+                                    title: "出错啦!",
+                                    text:content
+                                });
+                            }else{
+                                swal({
+                                    icon:"error",
+                                    title:data.result.msg
+                                })
+                            }
+
+                        }
+                    })
+                    .catch(error=>{
+                        console.error(error)
+                        swal({
+                            title:"请求出错",
+                            icon:"error"
+                        })
+                    })
+            },
+        }
+    }
+
+    Vue.createApp(app).mount("#vue-core-sign-login-username")
+}
+
+
+// 找回密码
+if(document.getElementById("vue-core-forgot-password")){
+    const app = {
+        data(){
+            return {
+                email:null,
+                captcha:null,
+                show:true,
+                YzCode:null,
+                sendCoded:true,
+                setpwd:true,
+                setPwd_password:null,
+                setPwd_cfpassword:null,
+                setPwd_submit:false,
+                password:null,
+            }
+        },
+        methods:{
+            sendCode(){
+                axios.post("/forgot-password/sendCode",{
+                    _token:csrf_token,
+                    email:this.email,
+                    captcha:this.captcha,
+                })
+                    .then(response=>{
+                        var data = response.data;
+                        if(data.success===true){
+                            swal({
+                                icon:"success",
+                                title:data.result.msg
+                            })
+                            this.show=!this.show;
+                        }else{
+                            if(data.result instanceof Array){
+                                var content = "";
+                                data.result.forEach(element => {
+                                    content = content+element+"\n"
+                                });
+                                swal({
+                                    icon : "error",
+                                    title: "出错啦!",
+                                    text:content
+                                });
+                            }else{
+                                swal({
+                                    icon:"error",
+                                    title:data.result.msg
+                                })
+                            }
+
+                        }
+                    })
+                    .catch(error=>{
+                        console.error(error)
+                        swal({
+                            title:"请求出错",
+                            icon:"error"
+                        })
+                    })
+            },
+            submit(){
+                axios.post("/forgot-password/verifyCode",{
+                    _token:csrf_token,
+                    code:this.YzCode,
+                })
+                    .then(response=>{
+                        var data = response.data;
+                        if(data.success===true){
+                            swal({
+                                icon:"success",
+                                title:data.result.msg
+                            })
+                            this.sendCoded=!this.sendCoded;
+                        }else{
+                            if(data.result instanceof Array){
+                                var content = "";
+                                data.result.forEach(element => {
+                                    content = content+element+"\n"
+                                });
+                                swal({
+                                    icon : "error",
+                                    title: "出错啦!",
+                                    text:content
+                                });
+                            }else{
+                                swal({
+                                    icon:"error",
+                                    title:data.result.msg
+                                })
+                            }
+
+                        }
+                    })
+                    .catch(error=>{
+                        console.error(error)
+                        swal({
+                            title:"请求出错",
+                            icon:"error"
+                        })
+                    })
+            },
+            // 设置新密码
+            setPwdSubmit(){
+                if(this.setPwd_password!==this.setPwd_cfpassword){
+                    swal({
+                        title:"两次输入密码不一致",
+                        icon:"error"
+                    })
+                }
+                axios.post("/forgot-password",{
+                    _token:csrf_token,
+                    cfpassword:this.setPwd_cfpassword,
+                    password:this.setPwd_password,
+                    code:this.YzCode,
+                    email:this.email
+                })
+                    .then(response=>{
+                        var data = response.data;
+                        if(data.success===true){
+                            swal({
+                                icon:"success",
+                                title:data.result.msg
+                            })
+                            this.setpwd=false;
+
+                        }else{
+                            if(data.result instanceof Array){
+                                var content = "";
+                                data.result.forEach(element => {
+                                    content = content+element+"\n"
+                                });
+                                swal({
+                                    icon : "error",
+                                    title: "出错啦!",
+                                    text:content
+                                });
+                            }else{
+                                swal({
+                                    icon:"error",
+                                    title:data.result.msg
+                                })
+                            }
+
+                        }
+                    })
+                    .catch(error=>{
+                        console.error(error)
+                        swal({
+                            title:"请求出错",
+                            icon:"error"
+                        })
+                    })
+            }
+        }
+    }
+
+    Vue.createApp(app).mount("#vue-core-forgot-password")
 }
