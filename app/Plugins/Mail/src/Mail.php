536835cfcd40c41fc7f62fd873247f83685beceb
zhuchunshu <zhuchunshu999@gmail.com>
2022-03-14 21:03:31 +0800

    更新:优化Mail插件

--- app/Plugins/Mail/src/Mail.php
+++ app/Plugins/Mail/src/Mail.php
@@ -82,7 +82,7 @@ class Mail
         $mail = new PHPMailer(true);
         try {
             //Server settings
-            $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+            $mail->SMTPDebug = SMTP::DEBUG_OFF;                      //Enable verbose debug output
             $mail->isSMTP();                                            //Send using SMTP
             $mail->Host       = $this->host;                     //Set the SMTP server to send through
             $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
