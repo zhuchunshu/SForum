78bda0e25ad903003cd72be0942e8dd5c6f7c828
zhuchunshu <zhuchunshu999@gmail.com>
2022-03-23 00:05:04 +0800

    更新:seo优化,修复已知bug

--- app/Plugins/Topic/src/Handler/Topic/CreateTopic.php
+++ app/Plugins/Topic/src/Handler/Topic/CreateTopic.php
@@ -41,7 +41,7 @@ class CreateTopic
         $hidden_user_list = $request->input("options_hidden_user_list");
         $summary = $request->input("summary");
         if(!$summary){
-            $summary = remove_bbCode(htmlspecialchars($html));
+            $summary = remove_bbCode(strip_tags($html));
         }
         if($hidden_user_class){
             $hidden_user_class = de_stringify($hidden_user_class);
