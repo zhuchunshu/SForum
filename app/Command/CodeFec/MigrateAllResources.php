1a684cee4e359ade9c0ea9411331ba29c8f2678f
zhuchunshu <laravel@88.com>
2022-04-04 19:56:14 +0800

    更新:修复已知迁移命令的bug

--- app/Command/CodeFec/MigrateAllResources.php
+++ app/Command/CodeFec/MigrateAllResources.php
@@ -34,54 +34,111 @@ class MigrateAllResources extends HyperfCommand
 
     public function handle()
     {
-        $plugins = getPath(plugin_path());
-        $rows = [];
-        $time = time();
-        foreach($plugins as $key=>$plugin){
-            $id = $key+1;
-            $plugin_name = $plugin;
-            if (is_dir(public_path("plugins/".$plugin_name))) {
-                if (!is_dir(plugin_path($plugin_name."/resources"))) {
-                     \Swoole\Coroutine\System::exec("mkdir " . plugin_path($plugin_name."/resources"));
-                }
-                if (!is_dir(plugin_path($plugin_name."/resources/assets"))) {
-                     \Swoole\Coroutine\System::exec("mkdir " . plugin_path($plugin_name."/resources/assets"));
-                }
-                copy_dir(public_path("plugins/" . $plugin_name),plugin_path($plugin_name . "/resources/assets"));
-            }
-
-            if (is_dir(BASE_PATH."/resources/js/plugins/".$plugin_name)) {
-                if (!is_dir(plugin_path($plugin_name."/resources"))) {
-                     \Swoole\Coroutine\System::exec("mkdir " . plugin_path($plugin_name."/resources"));
-                }
-                if (!is_dir(plugin_path($plugin_name."/resources/package"))) {
-                     \Swoole\Coroutine\System::exec("mkdir " . plugin_path($plugin_name."/resources/package"));
-                }
-                if (!is_dir(plugin_path($plugin_name."/resources/package/js"))) {
-                     \Swoole\Coroutine\System::exec("mkdir " . plugin_path($plugin_name."/resources/package/js"));
-                }
-                copy_dir(BASE_PATH."/resources/js/plugins/".$plugin_name,plugin_path($plugin_name . "/resources/package/js"));
-            }
-
-            if (is_dir(BASE_PATH."/resources/sass/plugins/".$plugin_name)) {
-                if (!is_dir(plugin_path($plugin_name."/resources"))) {
-                     \Swoole\Coroutine\System::exec("mkdir " . plugin_path($plugin_name."/resources"));
-                }
-                if (!is_dir(plugin_path($plugin_name."/resources/package"))) {
-                     \Swoole\Coroutine\System::exec("mkdir " . plugin_path($plugin_name."/resources/package"));
-                }
-                if (!is_dir(plugin_path($plugin_name."/resources/package/sass"))) {
-                     \Swoole\Coroutine\System::exec("mkdir " . plugin_path($plugin_name."/resources/package/sass"));
-                }
-                copy_dir(BASE_PATH."/resources/sass/plugins/".$plugin_name,plugin_path($plugin_name . "/resources/package/sass"));
-            }
-            $rows[]=[
-              $id,
-              $plugin,
-                plugin_path($plugin_name),
-                "<info>success</info>"
-            ];
-        }
-        $this->table(['插件id','被执行插件','插件路径','迁移状态'],$rows,'box-double');
+        $this->plugins();
+		$this->themes();
     }
+	
+	public function plugins(){
+		$plugins = getPath(plugin_path());
+		$rows = [];
+		$time = time();
+		foreach($plugins as $key=>$plugin){
+			$id = $key+1;
+			$plugin_name = $plugin;
+			if (is_dir(public_path("plugins/".$plugin_name))) {
+				if (!is_dir(plugin_path($plugin_name."/resources"))) {
+					\Swoole\Coroutine\System::exec("mkdir " . plugin_path($plugin_name."/resources"));
+				}
+				if (!is_dir(plugin_path($plugin_name."/resources/assets"))) {
+					\Swoole\Coroutine\System::exec("mkdir " . plugin_path($plugin_name."/resources/assets"));
+				}
+				copy_dir(public_path("plugins/" . $plugin_name),plugin_path($plugin_name . "/resources/assets"));
+			}
+			
+			if (is_dir(BASE_PATH."/resources/js/plugins/".$plugin_name)) {
+				if (!is_dir(plugin_path($plugin_name."/resources"))) {
+					\Swoole\Coroutine\System::exec("mkdir " . plugin_path($plugin_name."/resources"));
+				}
+				if (!is_dir(plugin_path($plugin_name."/resources/package"))) {
+					\Swoole\Coroutine\System::exec("mkdir " . plugin_path($plugin_name."/resources/package"));
+				}
+				if (!is_dir(plugin_path($plugin_name."/resources/package/js"))) {
+					\Swoole\Coroutine\System::exec("mkdir " . plugin_path($plugin_name."/resources/package/js"));
+				}
+				copy_dir(BASE_PATH."/resources/js/plugins/".$plugin_name,plugin_path($plugin_name . "/resources/package/js"));
+			}
+			
+			if (is_dir(BASE_PATH."/resources/sass/plugins/".$plugin_name)) {
+				if (!is_dir(plugin_path($plugin_name."/resources"))) {
+					\Swoole\Coroutine\System::exec("mkdir " . plugin_path($plugin_name."/resources"));
+				}
+				if (!is_dir(plugin_path($plugin_name."/resources/package"))) {
+					\Swoole\Coroutine\System::exec("mkdir " . plugin_path($plugin_name."/resources/package"));
+				}
+				if (!is_dir(plugin_path($plugin_name."/resources/package/sass"))) {
+					\Swoole\Coroutine\System::exec("mkdir " . plugin_path($plugin_name."/resources/package/sass"));
+				}
+				copy_dir(BASE_PATH."/resources/sass/plugins/".$plugin_name,plugin_path($plugin_name . "/resources/package/sass"));
+			}
+			$rows[]=[
+				$id,
+				$plugin,
+				plugin_path($plugin_name),
+				"<info>success</info>"
+			];
+		}
+		$this->table(['插件id','被执行插件','插件路径','迁移状态'],$rows,'box-double');
+	}
+	
+	public function themes(){
+		$plugins = getPath(theme_path());
+		$rows = [];
+		$time = time();
+		foreach($plugins as $key=>$plugin){
+			$id = $key+1;
+			$plugin_name = $plugin;
+			if (is_dir(theme_path("plugins/".$plugin_name))) {
+				if (!is_dir(theme_path($plugin_name."/resources"))) {
+					\Swoole\Coroutine\System::exec("mkdir " . theme_path($plugin_name."/resources"));
+				}
+				if (!is_dir(plugin_path($plugin_name."/resources/assets"))) {
+					\Swoole\Coroutine\System::exec("mkdir " . theme_path($plugin_name."/resources/assets"));
+				}
+				copy_dir(public_path("themes/" . $plugin_name),theme_path($plugin_name . "/resources/assets"));
+			}
+			
+			if (is_dir(BASE_PATH."/resources/js/themes/".$plugin_name)) {
+				if (!is_dir(theme_path($plugin_name."/resources"))) {
+					\Swoole\Coroutine\System::exec("mkdir " . theme_path($plugin_name."/resources"));
+				}
+				if (!is_dir(theme_path($plugin_name."/resources/package"))) {
+					\Swoole\Coroutine\System::exec("mkdir " . theme_path($plugin_name."/resources/package"));
+				}
+				if (!is_dir(plugin_path($plugin_name."/resources/package/js"))) {
+					\Swoole\Coroutine\System::exec("mkdir " . theme_path($plugin_name."/resources/package/js"));
+				}
+				copy_dir(BASE_PATH."/resources/js/themes/".$plugin_name,theme_path($plugin_name . "/resources/package/js"));
+			}
+			
+			if (is_dir(BASE_PATH."/resources/sass/themes/".$plugin_name)) {
+				if (!is_dir(theme_path($plugin_name."/resources"))) {
+					\Swoole\Coroutine\System::exec("mkdir " . theme_path($plugin_name."/resources"));
+				}
+				if (!is_dir(theme_path($plugin_name."/resources/package"))) {
+					\Swoole\Coroutine\System::exec("mkdir " . theme_path($plugin_name."/resources/package"));
+				}
+				if (!is_dir(theme_path($plugin_name."/resources/package/sass"))) {
+					\Swoole\Coroutine\System::exec("mkdir " . theme_path($plugin_name."/resources/package/sass"));
+				}
+				copy_dir(BASE_PATH."/resources/sass/themes/".$plugin_name,theme_path($plugin_name . "/resources/package/sass"));
+			}
+			$rows[]=[
+				$id,
+				$plugin,
+				theme_path($plugin_name),
+				"<info>success</info>"
+			];
+		}
+		$this->table(['主题id','被执行主题','主题路径','迁移状态'],$rows,'box-double');
+	}
 }
