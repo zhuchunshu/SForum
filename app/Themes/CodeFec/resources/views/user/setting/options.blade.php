83a128a87994c19ced175c0957427a6395b06637
zhuchunshu <laravel@88.com>
2022-05-18 13:40:03 +0800

    修复:已知用户灵活设置bug

--- app/Themes/CodeFec/resources/views/user/setting/options.blade.php
+++ app/Themes/CodeFec/resources/views/user/setting/options.blade.php
@@ -2,12 +2,12 @@
     <div class="row row-cards" id="users-settings-form">
         <div class="col-md-12">
             <!-- Cards with tabs component -->
-            @if(!count(Itf()->get('users_options')))
+            @if(count(Itf()->get('users_settings')))
                 <div class="card-tabs border-0">
                     <!-- Cards navigation -->
                     <ul class="nav nav-tabs">
-                        @foreach(Itf()->get('users_options') as $key=>$value)
-                            @if(core_Itf_id('users_options',$key)==1)
+                        @foreach(Itf()->get('users_settings') as $key=>$value)
+                            @if(core_Itf_id('users_settings',$key)==1)
                                 <li class="nav-item"><a href="#{{$key}}" class="nav-link active" data-bs-toggle="tab">{{$value['name']}}</a></li>
                             @else
                                 <li class="nav-item"><a href="#{{$key}}" class="nav-link" data-bs-toggle="tab">{{$value['name']}}</a></li>
@@ -16,8 +16,8 @@
                     </ul>
                     <div class="tab-content">
                         <!-- Content of card #1 -->
-                        @foreach(Itf()->get('users_options') as $key=>$value)
-                            @if(core_Itf_id('users_options',$key)==1)
+                        @foreach(Itf()->get('users_settings') as $key=>$value)
+                            @if(core_Itf_id('users_settings',$key)==1)
                                 <div id="{{$key}}" class="border-0 card tab-pane active show">
                                     @include($value['view'])
                                 </div>
@@ -36,7 +36,7 @@
                 </div>
             @endif
         </div>
-        @if(count(Itf()->get('users_options')))
+        @if(count(Itf()->get('users_settings')))
         <div class="p-3">
             <button @@click="submit" class="btn btn-light">提交</button>
         </div>
