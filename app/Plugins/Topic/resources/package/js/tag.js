01c74389521b363a642976ea8d7f672a6ecadd9b
zhuchunshu <zhuchunshu999@gmail.com>
2022-11-03 23:13:41 +0800

    更新:支持对用户创建标签审核

--- app/Plugins/Topic/resources/package/js/tag.js
+++ app/Plugins/Topic/resources/package/js/tag.js
@@ -1,30 +1,30 @@
 import axios from "axios";
 import swal from "sweetalert";
 
-if(document.getElementById("vue-topic-tag-table")){
+if (document.getElementById("vue-topic-tag-table")) {
     const vttt = {
         methods: {
-            remove(id){
-                axios.post("/admin/topic/tag/remove",{
-                    _token:csrf_token,
-                    id:id
+            remove(id) {
+                axios.post("/admin/topic/tag/remove", {
+                    _token: csrf_token,
+                    id: id
                 }).then(response => {
                     var data = response.data;
-                    if(data.success) {
+                    if (data.success) {
                         swal({
                             title: data.result.msg,
-                            icon:"success"
+                            icon: "success"
                         })
-                    }else{
+                    } else {
                         swal({
                             title: data.result.msg,
-                            icon:"error"
+                            icon: "error"
                         })
                     }
                 }).catch(error => {
                     swal({
-                        title:"请求出错,详细查看控制台",
-                        icon:"error"
+                        title: "请求出错,详细查看控制台",
+                        icon: "error"
                     })
                     console.error(error)
                 })
@@ -32,4 +32,75 @@ if(document.getElementById("vue-topic-tag-table")){
         }
     }
     Vue.createApp(vttt).mount("#vue-topic-tag-table");
+}
+
+if (document.getElementById("vue-topic-tag-jobs-table")) {
+    const app = {
+        methods: {
+            // 批准
+            approval(id) {
+                axios.post("/admin/topic/tag/job/approval", {
+                    _token: csrf_token,
+                    id: id
+                }).then(response => {
+                    var data = response.data;
+                    if (data.success) {
+                        swal({
+                            title: data.result.msg,
+                            icon: "success"
+                        })
+                    } else {
+                        swal({
+                            title: data.result.msg,
+                            icon: "error"
+                        })
+                    }
+                }).catch(error => {
+                    swal({
+                        title: "请求出错,详细查看控制台",
+                        icon: "error"
+                    })
+                    console.error(error)
+                })
+            },
+            // 驳回
+            reject(id) {
+                swal("驳回理由:", {
+                    content: "input",
+                    buttons: true
+                })
+                    .then((value) => {
+                        if (value !== null) {
+                            axios.post("/admin/topic/tag/job/reject", {
+                                _token: csrf_token,
+                                id: id,
+                                content:value
+                            }).then(response => {
+                                var data = response.data;
+                                if (data.success) {
+                                    swal({
+                                        title: data.result.msg,
+                                        icon: "success"
+                                    })
+                                } else {
+                                    swal({
+                                        title: data.result.msg,
+                                        icon: "error"
+                                    })
+                                }
+                            }).catch(error => {
+                                swal({
+                                    title: "请求出错,详细查看控制台",
+                                    icon: "error"
+                                })
+                                console.error(error)
+                            })
+                        }
+                        //swal(`You typed: ${value}`);
+                    });
+
+            }
+        }
+    }
+    Vue.createApp(app).mount("#vue-topic-tag-jobs-table");
 }
