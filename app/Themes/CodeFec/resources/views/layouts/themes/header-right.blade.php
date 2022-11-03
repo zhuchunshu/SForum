1cdfc67deb2feb5d3d16643b2b050c8ee2d22162
zhuchunshu <zhuchunshu@dog87.com>
2022-11-03 11:35:43 +0800

    更新:优化通知小红点

--- app/Themes/CodeFec/resources/views/layouts/themes/header-right.blade.php
+++ app/Themes/CodeFec/resources/views/layouts/themes/header-right.blade.php
@@ -15,7 +15,10 @@
         <a href="/user/notice" class="px-0 nav-link">
             <!-- Download SVG icon from http://tabler-icons.io/i/bell -->
             <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10 5a2 2 0 0 1 4 0a7 7 0 0 1 4 6v3a4 4 0 0 0 2 3h-16a4 4 0 0 0 2 -3v-3a7 7 0 0 1 4 -6" /><path d="M9 17v1a3 3 0 0 0 6 0v-1" /></svg>
-            <span id="core-notice-red" class="badge bg-red" style="display: none;"></span>
+            <span id="core-notice-red" class="badge bg-red badge-blink"  style="display: none;    position: absolute!important;
+    right: 0!important;
+    transform: translate(50%,-50%);
+    z-index: 1;"></span>
         </a>
     </div>
 
