b9ffd772b0da1025aa202fc1404575b7a1baf92b
zhuchunshu <laravel@88.com>
2022-12-08 22:50:41 +0800

    Publish:V2.1.2

--- build-info.php
+++ build-info.php
@@ -1,6 +1,15 @@
 <?php
+
+declare(strict_types=1);
+/**
+ * This file is part of zhuchunshu.
+ * @link     https://github.com/zhuchunshu
+ * @document https://github.com/zhuchunshu/super-forum
+ * @contact  laravel@88.com
+ * @license  https://github.com/zhuchunshu/super-forum/blob/master/LICENSE
+ */
 return [
-	'version' => 'v2.1.1',
-	'zhuchunshu' => 'zhuchunshu',
-	'link' => 'https://forum.runpod.cn'
+    'version' => 'v2.1.2',
+    'zhuchunshu' => 'zhuchunshu',
+    'link' => 'https://forum.runpod.cn',
 ];
