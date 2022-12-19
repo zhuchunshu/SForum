74d27eed2a20f61a6f4b5d76da6e2999f60ac48d
zhuchunshu <laravel@88.com>
2022-12-19 20:40:04 +0800

    更新:帖子页面边栏样式优化

--- app/Themes/CodeFec/resources/views/widget/topic/right_start_1.blade.php
+++ app/Themes/CodeFec/resources/views/widget/topic/right_start_1.blade.php
@@ -1,3 +1,17 @@
+
+{{--                作者信息--}}
+<div class="col-md-12">
+    <a class="card card-link" href="/users/{{$data->user->username}}.html">
+        <div class="card-cover card-cover-blurred text-center" style="background-image: url({{get_user_settings(auth()->id(),'backgroundImg','/plugins/Core/image/user_background.jpg')}})">
+            <span class="avatar avatar-xl avatar-thumb avatar-rounded" style="background-image: url({{super_avatar($data->user)}})"></span>
+        </div>
+        <div class="card-body text-center">
+            <div class="card-title mb-1">{{$data->user->username}}</div>
+            <div class="text-muted">本文作者，至今共发布{{$data->user->topic->count()}}篇文章</div>
+        </div>
+    </a>
+</div>
+
 {{--                标签信息--}}
 <div class="col-md-12">
     <a href="/tags/{{$data->tag->id}}.html" class="card card-link text-primary-fg" style="background-color: {{$data->tag->color}}!important;">
@@ -14,17 +28,6 @@
     </a>
 </div>
 
-{{--                作者信息--}}
-<div class="col-md-12">
-    <a class="card card-link" href="#">
-        <div class="card-cover card-cover-blurred text-center" style="background-image: url({{get_user_settings(auth()->id(),'backgroundImg','/plugins/Core/image/user_background.jpg')}})">
-            <span class="avatar avatar-xl avatar-thumb avatar-rounded" style="background-image: url({{super_avatar($data->user)}})"></span>
-        </div>
-        <div class="card-body text-center">
-            <div class="card-title mb-1">{{$data->user->username}}</div>
-            <div class="text-muted">本文作者，至今共发布{{$data->user->topic->count()}}篇文章</div>
-        </div>
-    </a>
-</div>
+
 
 
