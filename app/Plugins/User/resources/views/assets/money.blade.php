b9bb965ddd46ec3ef13d176291960cae7f584f74
zhuchunshu <laravel@88.com>
2022-12-23 01:00:06 +0800

    更新:优化余额明细页面在移动端的显示效果

--- app/Plugins/User/resources/views/assets/money.blade.php
+++ app/Plugins/User/resources/views/assets/money.blade.php
@@ -3,53 +3,55 @@
 @section('content')
 
     <div class="row row-cards">
-        <div class="card">
-            <div class="card-header">
-                <h3 class="card-title">账单明细</h3>
-                <div class="card-actions">
-                    <a href="/users/{{auth()->data()->username}}.html?m=users_home_menu_8">充值</a>
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">账单明细</h3>
+                    <div class="card-actions">
+                        <a href="/users/{{auth()->data()->username}}.html?m=users_home_menu_8">充值</a>
+                    </div>
                 </div>
-            </div>
-            <div class="card-body">
-                <table class="table table-responsive">
-                    <thead>
-                    <tr>
-                        <th>#</th>
-                        <th class="text-nowrap">变更前余额</th>
-                        <th class="text-nowrap">变更后余额</th>
-                        <th class="text-nowrap">绑定订单</th>
-                        <th class="text-nowrap">备注</th>
-                        <th class="text-nowrap">创建时间</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    @if(!$page->count())
+                <div class="card-body table-responsive">
+                    <table class="table table-vcenter">
+                        <thead>
                         <tr>
-                            <th>暂无结果</th>
-                            <td>暂无结果</td>
-                            <td>暂无结果</td>
-                            <td>暂无结果</td>
-                            <td>暂无结果</td>
-                            <td>暂无结果</td>
+                            <th>#</th>
+                            <th class="text-nowrap">变更前余额</th>
+                            <th class="text-nowrap">变更后余额</th>
+                            <th class="text-nowrap">绑定订单</th>
+                            <th class="text-nowrap">备注</th>
+                            <th class="text-nowrap">创建时间</th>
                         </tr>
-                    @else
-                        @foreach($page as $data)
+                        </thead>
+                        <tbody>
+                        @if(!$page->count())
                             <tr>
-                                <th>{{$data->id}}</th>
-                                <td>{{$data->original}} {{get_options('wealth_money_unit_name','元')}}</td>
-                                <td>{{$data->cash}} {{get_options('wealth_money_unit_name','元')}}</td>
-                                <td>@if($data->order_id) <a href="/user/order/{{$data->order_id}}.order">{{$data->order_id}}</a> @else 未绑定订单 @endif</td>
-                                <td>@if($data->remark) {{$data->remark}} @else 无备注 @endif</td>
-                                <td>{{$data->created_at}}</td>
+                                <th>暂无结果</th>
+                                <td>暂无结果</td>
+                                <td>暂无结果</td>
+                                <td>暂无结果</td>
+                                <td>暂无结果</td>
+                                <td>暂无结果</td>
                             </tr>
-                        @endforeach
-                    @endif
-                    </tbody>
-                </table>
-                <div class="mt-2">
-                    @if($page->count())
-                        {!! make_page($page) !!}
-                    @endif
+                        @else
+                            @foreach($page as $data)
+                                <tr>
+                                    <th>{{$data->id}}</th>
+                                    <td>{{$data->original}} {{get_options('wealth_money_unit_name','元')}}</td>
+                                    <td>{{$data->cash}} {{get_options('wealth_money_unit_name','元')}}</td>
+                                    <td>@if($data->order_id) <a href="/user/order/{{$data->order_id}}.order">{{$data->order_id}}</a> @else 未绑定订单 @endif</td>
+                                    <td>@if($data->remark) {{$data->remark}} @else 无备注 @endif</td>
+                                    <td>{{$data->created_at}}</td>
+                                </tr>
+                            @endforeach
+                        @endif
+                        </tbody>
+                    </table>
+                    <div class="mt-2">
+                        @if($page->count())
+                            {!! make_page($page) !!}
+                        @endif
+                    </div>
                 </div>
             </div>
         </div>
