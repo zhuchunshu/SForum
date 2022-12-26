d16b3ca3d5287b15815ddda289cbb91e6d775257
zhuchunshu <laravel@88.com>
2022-12-26 17:00:21 +0800

    更新:精简代码

--- app/Themes/CodeFec/resources/views/topic/show/content.blade.php
+++ app/Themes/CodeFec/resources/views/topic/show/content.blade.php
@@ -62,49 +62,42 @@
 
     {{--    上下页--}}
     @include('App::topic.show.include.lfpage')
-    @if(!isset($data->post->options->disable_comment))
+    @if(isset($data->post->options->disable_comment) && $data->post->options->disable_comment)
+        <div class="col-md-12">
+            <div class="border-0 card">
+                <div class="empty">
+                    <div class="empty-icon"><!-- Download SVG icon from http://tabler-icons.io/i/mood-sad -->
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-ban" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <circle cx="12" cy="12" r="9"></circle>
+                            <line x1="5.7" y1="5.7" x2="18.3" y2="18.3"></line>
+                        </svg>
+                    </div>
+                    <p class="empty-title">禁止评论</p>
+                    <p class="empty-subtitle text-muted">
+                        此帖子关闭了评论及回复功能
+                    </p>
+                    @if(!auth()->check())
+                        <div class="empty-action">
+                            <a href="/login" class="btn btn-primary">
+                                <!-- Download SVG icon from http://tabler-icons.io/i/search -->
+                                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-login" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                    <path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2"></path>
+                                    <path d="M20 12h-13l3 -3m0 6l-3 -3"></path>
+                                </svg>
+                                登陆
+                            </a>
+                        </div>
+                    @endif
+                </div>
+            </div>
+        </div>
+    @else
         {{--    显示评论--}}
         @include('Comment::Widget.show-topic')
         {{--    评论--}}
         @include('Comment::Widget.topic')
-    @else
-        @if(@$data->post->options->disable_comment)
-            <div class="col-md-12">
-                <div class="border-0 card">
-                    <div class="empty">
-                        <div class="empty-icon"><!-- Download SVG icon from http://tabler-icons.io/i/mood-sad -->
-                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-ban" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                <circle cx="12" cy="12" r="9"></circle>
-                                <line x1="5.7" y1="5.7" x2="18.3" y2="18.3"></line>
-                            </svg>
-                        </div>
-                        <p class="empty-title">禁止评论</p>
-                        <p class="empty-subtitle text-muted">
-                            此帖子关闭了评论及回复功能
-                        </p>
-                        @if(!auth()->check())
-                            <div class="empty-action">
-                                <a href="/login" class="btn btn-primary">
-                                    <!-- Download SVG icon from http://tabler-icons.io/i/search -->
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-login" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                        <path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2"></path>
-                                        <path d="M20 12h-13l3 -3m0 6l-3 -3"></path>
-                                    </svg>
-                                    登陆
-                                </a>
-                            </div>
-                        @endif
-                    </div>
-                </div>
-            </div>
-        @else
-            {{--    显示评论--}}
-            @include('Comment::Widget.show-topic')
-            {{--    评论--}}
-            @include('Comment::Widget.topic')
-        @endif
     @endif
 
     @if(auth()->check())
