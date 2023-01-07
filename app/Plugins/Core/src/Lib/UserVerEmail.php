e94a170a6fa025f23f53f6f74b267533d16e4a51
zhuchunshu <laravel@88.com>
2023-01-07 23:46:41 +0800

    优化:邮箱验证码改为数字验证码

--- app/Plugins/Core/src/Lib/UserVerEmail.php
+++ app/Plugins/Core/src/Lib/UserVerEmail.php
@@ -1,59 +1,70 @@
 <?php
 
+declare(strict_types=1);
+/**
+ * This file is part of zhuchunshu.
+ * @link     https://github.com/zhuchunshu
+ * @document https://github.com/zhuchunshu/super-forum
+ * @contact  laravel@88.com
+ * @license  https://github.com/zhuchunshu/super-forum/blob/master/LICENSE
+ */
 namespace App\Plugins\Core\src\Lib;
 
 use App\Plugins\User\src\Models\User;
 
 class UserVerEmail
 {
-    public function make(){
+    public function make()
+    {
         $id = auth()->data()->id;
-        cache()->set("core.user.ver.email.".$id,\Hyperf\Utils\Str::random(7),1800);
-        cache()->set("core.user.ver.email.time.".$id,time()+60);
-        return cache()->get("core.user.ver.email.".$id);
+        cache()->set('core.user.ver.email.' . $id, random_int(100000, 999999), 1800);
+        cache()->set('core.user.ver.email.time.' . $id, time() + 60);
+        return cache()->get('core.user.ver.email.' . $id);
     }
 
     public function check($captcha): bool
     {
         $id = auth()->data()->id;
-        if(!cache()->has("core.user.ver.email.".$id)){
+        if (! cache()->has('core.user.ver.email.' . $id)) {
             return false;
         }
-        $data = cache()->get("core.user.ver.email.".$id);
-	    return $captcha === $data;
+        $data = cache()->get('core.user.ver.email.' . $id);
+        return $captcha === $data;
     }
 
     public function ifsend(): bool
     {
         $id = auth()->id();
-        if(!cache()->has("core.user.ver.email.time.".$id)){
+        if (! cache()->has('core.user.ver.email.time.' . $id)) {
             return true;
         }
-        if(cache()->get("core.user.ver.email.time.".$id,0)-time()<=0){
+        if (cache()->get('core.user.ver.email.time.' . $id, 0) - time() <= 0) {
             return true;
         }
         return false;
     }
 
-    public function sendTime(): int{
+    public function sendTime(): int
+    {
         $id = auth()->id();
-        return cache()->get("core.user.ver.email.time.".$id,0)-time();
+        return cache()->get('core.user.ver.email.time.' . $id, 0) - time();
     }
 
-    public function send($id){
-		$data = User::query()->where('id',$id)->first();
-		$username = $data->username;
-		$email = $data->email;
-	    $captcha = $this->make();
+    public function send($id)
+    {
+        $data = User::query()->where('id', $id)->first();
+        $username = $data->username;
+        $email = $data->email;
+        $captcha = $this->make();
         $mail = Email();
-        go(function()use ($mail,$email,$captcha,$username){
-	        $mail->addAddress($email);
-	        $mail->Subject = "【".get_options("web_name")."】请查看你的邮箱验证码";
-	        $mail->Body    = <<<HTML
+        go(function () use ($mail, $email, $captcha, $username) {
+            $mail->addAddress($email);
+            $mail->Subject = '【' . get_options('web_name') . '】请查看你的邮箱验证码';
+            $mail->Body = <<<HTML
 你好 {$username},<br>
 你的邮箱验证码是:{$captcha}
 HTML;
-	        $mail->send();
+            $mail->send();
         });
         return $captcha;
     }
