a71daacf1c9971076a84fa611f993a8b5407bf07
zhuchunshu <laravel@88.com>
2023-01-09 21:00:55 +0800

    优化:移动端页头登陆icon优化

--- app/Themes/CodeFec/resources/views/layouts/themes/header-right.blade.php
+++ app/Themes/CodeFec/resources/views/layouts/themes/header-right.blade.php
@@ -80,12 +80,11 @@
         </span>
         <span class="d-block d-md-none">
             <a class="px-0 nav-link" href="/login">
-                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-login" width="24"
-                     height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none"
-                     stroke-linecap="round" stroke-linejoin="round">
+<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user-circle" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-   <path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2"></path>
-   <path d="M20 12h-13l3 -3m0 6l-3 -3"></path>
+   <circle cx="12" cy="12" r="9"></circle>
+   <circle cx="12" cy="10" r="3"></circle>
+   <path d="M6.168 18.849a4 4 0 0 1 3.832 -2.849h4a4 4 0 0 1 3.834 2.855"></path>
 </svg>
             </a>
         </span>
