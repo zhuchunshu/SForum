ba83b0d979b0c7275ef56f1708df337bc5f00695
zhuchunshu <laravel@88.com>
2023-01-11 00:26:09 +0800

    更新:优化插件页面

--- resources/views/admin/plugins/index.blade.php
+++ resources/views/admin/plugins/index.blade.php
@@ -11,6 +11,21 @@
 
 @section('content')
     <div class="row row-cards">
+        <div class="col-lg-12">
+            <div class="alert alert-info alert-dismissible" role="alert">
+                <div class="d-flex">
+                    <div>
+                        <!-- Download SVG icon from http://tabler-icons.io/i/info-circle -->
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon alert-icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><circle cx="12" cy="12" r="9" /><line x1="12" y1="8" x2="12.01" y2="8" /><polyline points="11 12 12 12 12 16 13 16" /></svg>
+                    </div>
+                    <div>
+                        <h4 class="alert-title">Did you know?</h4>
+                        <div class="text-muted">插件启停时网站出现短暂502为正常现象，因为SuperForum在优化/重建代理类，这个过程一般需要5秒左右</div>
+                    </div>
+                </div>
+                <a class="btn-close" data-bs-dismiss="alert" aria-label="close"></a>
+            </div>
+        </div>
         @foreach (\App\CodeFec\Plugins::GetAll() as $key => $value)
             <div class="col-md-4">
                 <div class="card">
