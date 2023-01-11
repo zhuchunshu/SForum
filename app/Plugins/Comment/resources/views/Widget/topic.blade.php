2eb465b3c1f16095031cf89db52a6a8b600242b3
zhuchunshu <laravel@88.com>
2023-01-11 18:34:01 +0800

    更新:优化评论缓存

--- app/Plugins/Comment/resources/views/Widget/topic.blade.php
+++ app/Plugins/Comment/resources/views/Widget/topic.blade.php
@@ -63,14 +63,6 @@
 <script>
     var reg =  new RegExp('<[^>]+>','gi');  //过滤所有的html标签，不包括内容
 
-    // var reg2 = /<(img|br|hr|input)[^>]*>/gi;  //只匹配img、br、hr、input标签
-    var reg2 = new RegExp('<(img|br|hr|input)[^>]*>','gi');  //只匹配img、br、hr、input标签
-
-    // var reg3 = /<(\S*)[^>]*>[^<]*<\/(\1)>/gi;        //分组匹配，过滤所有的html标签，包括内容
-    var reg3 = new RegExp('<(\\S*)[^>]*>[^<]*<\\/(\\1)>','gi');  //分组匹配，过滤所有的html标签，包括内容
-
-
-
     /*
     * 将所有的标签过滤，不过滤标签内内容
     * */
@@ -82,27 +74,6 @@
         return str.replace(reg,'');
     }
 
-    /*
-    * 讲所有的标签过滤，也过滤标签内的内容
-    * str 需要过滤的字符串
-    * isbool  为false则需要单标签过滤，为true则不需要单标签过滤
-    * */
-    function filterHtmlOrContainer(str,isbool) {
-        if(typeof str !='string'){  //不是字符串
-            return str;
-        }
-        var result = str;
-        if(!isbool){  //先把单标签过滤了
-            result = result.replace(reg2, '');
-        }
-        result = result.replace(reg3,'');    //先经过分组匹配，把双标签去除，如果是嵌套标签，则会先将嵌套标签内的双标签过滤掉
-        if(reg3.test(result)) { //如果为true，则代表还有标签
-            return filterHtmlOrContainer(result, true);
-        }else {
-            return result;
-        }
-    }
-
     document.addEventListener('alpine:init', () => {
         Alpine.data('comment', () => ({
             init() {
@@ -112,7 +83,7 @@
             },
             content:(()=>{
                 if(localStorage.getItem('create_topic_comment_{{$data->id}}')){
-                    return localStorage.getItem('create_topic_comment_{{$data->id}}')
+                    return filterHtml(localStorage.getItem('create_topic_comment_{{$data->id}}'))
                 }
                 return null;
             })
