a8e8057f702c45c11da2de84c3ef527c1ec69b65
zhuchunshu <laravel@88.com>
2023-01-22 00:48:50 +0800

    更新:支持关闭enable_static_handler

--- config/autoload/server.php
+++ config/autoload/server.php
@@ -26,21 +26,21 @@ return [
                 Event::ON_REQUEST => [CodeFecServer::class, 'onRequest'],
             ],
         ],
-//        [
-//            'name' => 'socket-io',
-//            'type' => ServerInterface::SERVER_WEBSOCKET,
-//            'host' => (string) env('SERVER_WEB_DOMAIN', '0.0.0.0'),
-//            'port' => (int) env('SERVER_WS_PORT', 9502),
-//            'sock_type' => SWOOLE_SOCK_TCP,
-//            'callbacks' => [
-//                Event::ON_HAND_SHAKE => [Hyperf\WebSocketServer\Server::class, 'onHandShake'],
-//                Event::ON_MESSAGE => [Hyperf\WebSocketServer\Server::class, 'onMessage'],
-//                Event::ON_CLOSE => [Hyperf\WebSocketServer\Server::class, 'onClose'],
-//            ],
-//            'settings' => [
-//                'open_websocket_protocol' => false,
-//            ],
-//        ],
+        //        [
+        //            'name' => 'socket-io',
+        //            'type' => ServerInterface::SERVER_WEBSOCKET,
+        //            'host' => (string) env('SERVER_WEB_DOMAIN', '0.0.0.0'),
+        //            'port' => (int) env('SERVER_WS_PORT', 9502),
+        //            'sock_type' => SWOOLE_SOCK_TCP,
+        //            'callbacks' => [
+        //                Event::ON_HAND_SHAKE => [Hyperf\WebSocketServer\Server::class, 'onHandShake'],
+        //                Event::ON_MESSAGE => [Hyperf\WebSocketServer\Server::class, 'onMessage'],
+        //                Event::ON_CLOSE => [Hyperf\WebSocketServer\Server::class, 'onClose'],
+        //            ],
+        //            'settings' => [
+        //                'open_websocket_protocol' => false,
+        //            ],
+        //        ],
     ],
     'settings' => [
         Constant::OPTION_ENABLE_COROUTINE => true,
@@ -55,7 +55,7 @@ return [
         Constant::OPTION_PACKAGE_MAX_LENGTH => 100 * 1024 * 1024,
         // 静态资源
         'document_root' => BASE_PATH . '/public',
-        'enable_static_handler' => true,
+        'enable_static_handler' => env('ENABLE_STATIC_HANDLER', true),
     ],
     'callbacks' => [
         Event::ON_WORKER_START => [Hyperf\Framework\Bootstrap\WorkerStartCallback::class, 'onWorkerStart'],
