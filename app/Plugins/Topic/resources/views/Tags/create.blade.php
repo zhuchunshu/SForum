664761064eda2ac44334cf2f826612c5ec6aa3b9
zhuchunshu <laravel@88.com>
2023-01-24 20:01:03 +0800

    Fix:修复前台修改标签图标报错的bug

--- app/Plugins/Topic/resources/views/Tags/create.blade.php
+++ app/Plugins/Topic/resources/views/Tags/create.blade.php
@@ -3,54 +3,68 @@
 
 @section('content')
     <div class="col-md-12">
-        <div class="card card-body" id="vue-user-topic-tag-create">
-            <h3 class="card-title">{{__("tag.create tag")}}</h3>
-            <form method="post" action="/tags/create?Redirect=/tags/create" @@submit.prevent="submit" enctype="multipart/form-data">
-                <x-csrf/>
-                <div class="mb-3">
-                    <label class="form-label">
-                        {{__("app.name")}}
-                    </label>
-                    <input type="text" class="form-control" name="name" v-model="name" required>
-                </div>
-                <div class="mb-3">
-                    <div class="row">
-                        <div class="col-6">
-                            <label class="form-label">
-                                {{__("app.color")}}
-                            </label>
-                            <input type="color" class="form-control form-control-color" name="color" v-model="color" required>
+        <div class="card" id="vue-user-topic-tag-create">
+            <div class="card-header">
+                <ol class="breadcrumb breadcrumb-arrows" aria-label="breadcrumbs">
+                    <li class="breadcrumb-item"><a href="/">首页</a></li>
+                    <li class="breadcrumb-item">
+                        <a href="/tags">标签列表</a>
+                    </li>
+                    <li class="breadcrumb-item active" aria-current="page"><a href="#">创建</a></li>
+                </ol>
+            </div>
+            <div class="card-body">
+                <h3 class="card-title">{{__("tag.create tag")}}</h3>
+                <form method="post" action="/tags/create?Redirect=/tags/create" @@submit.prevent="submit"
+                      enctype="multipart/form-data">
+                    <x-csrf/>
+                    <div class="mb-3">
+                        <label class="form-label">
+                            {{__("app.name")}}
+                        </label>
+                        <input type="text" class="form-control" name="name" v-model="name" required>
+                    </div>
+                    <div class="mb-3">
+                        <div class="row">
+                            <div class="col-6">
+                                <label class="form-label">
+                                    {{__("app.color")}}
+                                </label>
+                                <input type="color" class="form-control form-control-color" name="color" v-model="color"
+                                       required>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="mb-3">
-                    <label class="form-label">
-                        {{__("app.icon")}}
-                    </label>
-                    <a href="https://tabler-icons.io/">https://tabler-icons.io/</a>
-                    <textarea name="icon" v-model="icon" rows="10" class="form-control" required></textarea>
-                </div>
+                    <div class="mb-3">
+                        <label class="form-label">
+                            {{__("app.icon")}}
+                        </label>
+                        <a href="https://tabler-icons.io/">https://tabler-icons.io/</a>
+                        <textarea name="icon" v-model="icon" rows="10" class="form-control" required></textarea>
+                    </div>
 
-                <div class="mb-3">
-                    <label class="form-label"> {{__("tag.Which user group can use this label")}} </label>
-                    @foreach($userClass as $value)
-                        <div class="col-4">
-                            <label class="form-check form-switch">
-                                <input name="userClass[]" value="{{$value['name']}}" class="form-check-input" type="checkbox">
-                                <span class="form-check-label">{{$value['name']}}</span>
-                            </label>
-                        </div>
-                    @endforeach
-                    <small style="color:red">{{__("tag.If not selected, this label is available to all user groups")}}</small>
-                </div>
-                <div class="mb-3">
-                    <label class="form-label">{{__("app.description")}}</label>
-                    <textarea name="description" class="form-control" rows="4"></textarea>
-                </div>
-                <div class="mb-3">
-                    <button class="btn btn-primary">{{__("app.submit")}}</button>
-                </div>
-            </form>
+                    <div class="mb-3">
+                        <label class="form-label"> {{__("tag.Which user group can use this label")}} </label>
+                        @foreach($userClass as $value)
+                            <div class="col-4">
+                                <label class="form-check form-switch">
+                                    <input name="userClass[]" value="{{$value['name']}}" class="form-check-input"
+                                           type="checkbox">
+                                    <span class="form-check-label">{{$value['name']}}</span>
+                                </label>
+                            </div>
+                        @endforeach
+                        <small style="color:red">{{__("tag.If not selected, this label is available to all user groups")}}</small>
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">{{__("app.description")}}</label>
+                        <textarea name="description" class="form-control" rows="4"></textarea>
+                    </div>
+                    <div class="mb-3">
+                        <button class="btn btn-primary">{{__("app.submit")}}</button>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
 @endsection
--- app/Plugins/Topic/resources/views/Tags/edit.blade.php
+++ app/Plugins/Topic/resources/views/Tags/edit.blade.php
@@ -3,50 +3,70 @@
 
 @section('content')
     <div class="col-md-12">
-        <div class="card card-body" id="vue-topic-tag-edit">
-            <h3 class="card-title">{{__("tag.edit tag")}}:{{$data->name}}</h3>
-            <form method="post" action="/tags/edit?Redirect=/tags/{{$data->id}}/edit" @@submit.prevent="submit" enctype="multipart/form-data">
-                <x-csrf/>
-                <div class="mb-3">
-                    <label class="form-label">
-                        {{__("app.name")}}
-                    </label>
-                    <input type="text" class="form-control" value="{{$data->name}}" name="name" v-model="name" required>
-                </div>
-                <div class="mb-3">
-                    <div class="row">
-                        <div class="col-6">
-                            <label class="form-label">
-                                {{__("app.color")}}
-                            </label>
-                            <input type="color" {{$value->color}} class="form-control form-control-color" name="color" v-model="color" required>
+        <div class="card" id="vue-topic-tag-edit">
+            <div class="card-header">
+                <ol class="breadcrumb breadcrumb-arrows" aria-label="breadcrumbs">
+                    <li class="breadcrumb-item"><a href="/">首页</a></li>
+                    <li class="breadcrumb-item">
+                        <a href="/tags">标签列表</a>
+                    </li>
+                    <li class="breadcrumb-item"><a href="/tags/{{$data->id}}.html">
+                            {!! $data->icon !!}{{$data->name}}
+                        </a>
+                    </li>
+                    <li class="breadcrumb-item active" aria-current="page"><a href="#">修改</a></li>
+                </ol>
+            </div>
+            <div class="card-body">
+                <h3 class="card-title">{{__("tag.edit tag")}}:{{$data->name}}</h3>
+                <form method="post" action="/tags/edit?Redirect=/tags/{{$data->id}}/edit" @@submit.prevent="submit"
+                      enctype="multipart/form-data">
+                    <x-csrf/>
+                    <div class="mb-3">
+                        <label class="form-label">
+                            {{__("app.name")}}
+                        </label>
+                        <input type="text" class="form-control" value="{{$data->name}}" name="name" v-model="name"
+                               required>
+                    </div>
+                    <div class="mb-3">
+                        <div class="row">
+                            <div class="col-6">
+                                <label class="form-label">
+                                    {{__("app.color")}}
+                                </label>
+                                <input type="color" {{$data->color}} class="form-control form-control-color"
+                                       name="color" v-model="color" required>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="mb-3">
-                    <label class="form-label">
-                        {{__("app.icon")}}
-                    </label>
-                    <a href="https://tabler-icons.io/">https://tabler-icons.io/</a>
-                    <textarea name="icon" v-model="icon" rows="10" class="form-control" required>{{$value->icon}}</textarea>
-                </div>
-                <div class="mb-3">
-                    <label class="form-label"> {{__("tag.Which user group can use this label")}}? </label>
-                    <select class="form-select" name="userClass[]" multiple size="8">
-                        @foreach($userClass as $value)
-                            <option value="{{$value->name}}" @if(user_DeCheckClass($data,$value->name)) selected @endif>{{$value->name}}</option>
-                        @endforeach
-                    </select>
-                    <small style="color:red">{{__("tag.If not selected, this label is available to all user groups")}}</small>
-                </div>
-                <div class="mb-3">
-                    <label class="form-label">{{__("app.description")}}</label>
-                    <textarea name="description" class="form-control" rows="4">{{$data->description}}</textarea>
-                </div>
-                <div class="mb-3">
-                    <button name="id" value="{{$data->id}}" class="btn btn-primary">{{__("app.submit")}}</button>
-                </div>
-            </form>
+                    <div class="mb-3">
+                        <label class="form-label">
+                            {{__("app.icon")}}
+                        </label>
+                        <a href="https://tabler-icons.io/">https://tabler-icons.io/</a>
+                        <textarea name="icon" v-model="icon" rows="10" class="form-control"
+                                  required>{{$data->icon}}</textarea>
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label"> {{__("tag.Which user group can use this label")}}? </label>
+                        <select class="form-select" name="userClass[]" multiple size="8">
+                            @foreach($userClass as $value)
+                                <option value="{{$value->name}}"
+                                        @if(user_DeCheckClass($data,$value->name)) selected @endif>{{$value->name}}</option>
+                            @endforeach
+                        </select>
+                        <small style="color:red">{{__("tag.If not selected, this label is available to all user groups")}}</small>
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">{{__("app.description")}}</label>
+                        <textarea name="description" class="form-control" rows="4">{{$data->description}}</textarea>
+                    </div>
+                    <div class="mb-3">
+                        <button name="id" value="{{$data->id}}" class="btn btn-primary">{{__("app.submit")}}</button>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
 @endsection
