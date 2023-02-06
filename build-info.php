2d978a9fdb007ab42ec98d7e561d3e19be0075c3
zhuchunshu <laravel@88.com>
2023-02-06 19:32:42 +0800

    Publish:V2.2.11

--- build-info.php
+++ build-info.php
@@ -9,7 +9,7 @@ declare(strict_types=1);
  * @license  https://github.com/zhuchunshu/SForum/blob/master/LICENSE
  */
 return [
-    'version' => 'v2.2.9',
+    'version' => 'v2.2.11',
     'author' => 'zhuchunshu',
     'link' => 'https://www.runpod.cn',
 ];
