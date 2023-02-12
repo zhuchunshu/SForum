ba1af14f42ffc3c0c3eb8a1824ae555a240d1c01
zhuchunshu <laravel@88.com>
2023-02-12 14:50:17 +0800

    更新:评论'仅楼主可见样式优化'

--- app/Plugins/Comment/resources/views/Widget/only-author.blade.php
+++ app/Plugins/Comment/resources/views/Widget/only-author.blade.php
@@ -1,5 +1,5 @@
 <div core-show="comment" comment-id="{{$value->id}}"
-     class="col-md-12 markdown">
+     class="col-md-12 markdown p-2">
     <div class="card card-body">
         此评论仅楼主可见
     </div>
