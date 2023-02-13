c171d85d541447fd511bc2c110bb00974ac894fb
zhuchunshu <laravel@88.com>
2023-02-13 15:57:28 +0800

    更新:视图优化

--- app/Plugins/User/resources/views/setting/user/code.blade.php
+++ app/Plugins/User/resources/views/setting/user/code.blade.php
@@ -3,9 +3,5 @@
         <label class="form-label">主页自定义css代码
         </label>
         <textarea class="form-control" v-model="data.home_css_code" id="" rows="10"></textarea>
-        <small>默认开启</small>
     </div>
-
-
-
 </div>
--- app/Themes/CodeFec/resources/views/index/index.blade.php
+++ app/Themes/CodeFec/resources/views/index/index.blade.php
@@ -42,8 +42,16 @@
             @endforeach
             <li class="nav-item ms-auto">
                 <div class="dropdown">
-                    <a href="#" class="btn-action dropdown-toggle" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><!-- Download SVG icon from http://tabler-icons.io/i/dots-vertical -->
-                        <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><circle cx="12" cy="12" r="1" /><circle cx="12" cy="19" r="1" /><circle cx="12" cy="5" r="1" /></svg>
+                    <a href="#" class="btn-action dropdown-toggle" data-bs-toggle="dropdown" aria-haspopup="true"
+                       aria-expanded="false"><!-- Download SVG icon from http://tabler-icons.io/i/dots-vertical -->
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24"
+                             stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
+                             stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                            <circle cx="12" cy="12" r="1"/>
+                            <circle cx="12" cy="19" r="1"/>
+                            <circle cx="12" cy="5" r="1"/>
+                        </svg>
                     </a>
                     <div class="dropdown-menu dropdown-menu-end">
                         @foreach(Itf()->get('ui-home-tabs-dropdown') as $k=>$v)
@@ -61,16 +69,14 @@
         @foreach($page as $data)
             @include('App::index.style2')
         @endforeach
+        <div class="mt-2">
+            {!! make_page($page) !!}
+        </div>
     @else
         <div class="col-md-12">
             <div class="border-0 card card-body">
                 <div class="text-center card-title">{{__("app.No more results")}}</div>
             </div>
-        </div>
-    @endif
-    @if($page->count())
-        <div class="mt-2">
-            {!! make_page($page) !!}
         </div>
     @endif
 </div>
