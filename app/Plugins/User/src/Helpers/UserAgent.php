de1f4cfe11399175fc22ba9050e9f8351dd1351d
zhuchunshu <laravel@88.com>
2023-02-13 20:56:33 +0800

    更新:重写UserAgent解析类,优化登陆设备信息显示

--- app/Plugins/User/src/Helpers/UserAgent.php
+++ app/Plugins/User/src/Helpers/UserAgent.php
@@ -10,81 +10,136 @@ declare(strict_types=1);
  */
 namespace App\Plugins\User\src\Helpers;
 
+use WhichBrowser\Parser;
+
 class UserAgent
 {
     public static function getBrowser($agent): string
     {
-        if (preg_match('/MSIE\s([^\s|;]+)/i', $agent, $regs)) {
-            $outputer = '<i class="ua-icon icon-ie"></i>Internet Explore';
-        } elseif (preg_match('/FireFox\/([^\s]+)/i', $agent, $regs)) {
-            $outputer = '<i class="ua-icon icon-firefox"></i>FireFox';
-        } elseif (preg_match('/Maxthon([\d]*)\/([^\s]+)/i', $agent, $regs)) {
-            $outputer = '<i class="ua-icon icon-edge"></i>MicroSoft Edge';
-        } elseif (preg_match('#360([a-zA-Z0-9.]+)#i', $agent, $regs)) {
-            $outputer = '<i class="ua-icon icon-360"></i>360极速浏览器';
-        } elseif (preg_match('/Edge([\d]*)\/([^\s]+)/i', $agent, $regs)) {
-            $outputer = '<i class="ua-icon icon-edge"></i>MicroSoft Edge';
-        } elseif (stripos($agent, 'UC') !== false) {
-            $outputer = '<i class="ua-icon icon-uc"></i>UC浏览器';
-        } elseif (preg_match('/QQ/i', $agent, $regs) || preg_match('/QQBrowser\/([^\s]+)/i', $agent, $regs)) {
-            $outputer = '<i class= "ua-icon icon-qq"></i>QQ浏览器';
-        } elseif (preg_match('/UBrowser/i', $agent, $regs)) {
-            $outputer = '<i class="ua-icon icon-uc"></i>UC浏览器';
-        } elseif (preg_match('/Opera[\s|\/]([^\s]+)/i', $agent, $regs)) {
-            $outputer = '<i class= "ua-icon icon-opera"></i>Opera';
-        } elseif (preg_match('/Chrome([\d]*)\/([^\s]+)/i', $agent, $regs)) {
-            $outputer = '<i class="ua-icon icon-chrome""></i>Google Chrome';
-        } elseif (preg_match('/safari\/([^\s]+)/i', $agent, $regs)) {
-            $outputer = '<i class="ua-icon icon-safari"></i>Safari';
-        } else {
-            $outputer = '<i class="ua-icon icon-chrome"></i>Google Chrome';
+        $parse = new Parser($agent);
+        // 默认输出为null
+        $echo = null;
+
+        // chrome
+        if ($parse->browser->name === 'Chrome') {
+            $echo = '<i class="ua-icon icon-chrome"></i>Chrome';
         }
-        return $outputer;
+
+        //wechat
+        if ($parse->browser->name === 'WeChat') {
+            $echo = '<i class="ua-icon icon-wechat"></i>WeChat';
+        }
+        // Internet Explore
+        if ($parse->browser->name === 'Internet Explorer') {
+            $echo = '<i class="ua-icon icon-ie"></i>IE';
+        }
+        // FireFox
+        if ($parse->browser->name === 'Firefox') {
+            $echo = '<i class="ua-icon icon-firefox"></i>FireFox';
+        }
+        // Edge
+        if ($parse->browser->name === 'Edge') {
+            $echo = '<i class="ua-icon icon-edge"></i>Edge';
+        }
+        // 360极速浏览器
+        if ($parse->browser->name === '360SE') {
+            $echo = '<i class="ua-icon icon-360"></i>360极速浏览器';
+        }
+        // UC浏览器
+        if ($parse->browser->name === 'UC Browser') {
+            $echo = '<i class="ua-icon icon-uc"></i>UC浏览器';
+        }
+        // QQ浏览器
+        if ($parse->browser->name === 'QQ Browser') {
+            $echo = '<i class="ua-icon icon-qq"></i>QQ浏览器';
+        }
+        // Opera
+        if ($parse->browser->name === 'Opera') {
+            $echo = '<i class="ua-icon icon-opera"></i>Opera';
+        }
+        // Safari
+        if ($parse->browser->name === 'Safari') {
+            $echo = '<i class="ua-icon icon-safari"></i>Safari';
+        }
+
+        // 猎豹浏览器
+        if ($parse->browser->name === 'LBBROWSER') {
+            $echo = '<i class="ua-icon icon-liebao"></i>猎豹浏览器';
+        }
+
+        // 百度浏览器
+        if ($parse->browser->name === 'Baidu') {
+            $echo = '<i class="ua-icon icon-baidu"></i>百度浏览器';
+        }
+
+        // 小米浏览器
+        if ($parse->browser->name === 'MIUI Browser') {
+            $echo = '<i class="ua-icon icon-xiaomi"></i>小米浏览器';
+        }
+
+
+        if ($echo === null) {
+            $echo = $parse->browser->name;
+        }
+        return $echo ?: $parse->toString();
     }
 
     // 获取操作系统信息
     public static function getOs($agent): string
     {
-        $os = false;
+        $parse = new Parser($agent);
+        // 默认输出为null
+        $echo = null;
 
-        if (stripos($agent, 'win') !== false) {
-            if (preg_match('/nt 6.0/i', $agent)) {
-                $os = '<i class= "ua-icon icon-win1"></i>Windows Vista';
-            } elseif (preg_match('/nt 6.1/i', $agent)) {
-                $os = '<i class= "ua-icon icon-win1"></i>Windows 7';
-            } elseif (preg_match('/nt 6.2/i', $agent)) {
-                $os = '<i class="ua-icon icon-win2"></i>Windows 8';
-            } elseif (preg_match('/nt 6.3/i', $agent)) {
-                $os = '<i class= "ua-icon icon-win2"></i>Windows 8.1';
-            } elseif (preg_match('/nt 5.1/i', $agent)) {
-                $os = '<i class="ua-icon icon-win1"></i>Windows XP';
-            } elseif (preg_match('/nt 10.0/i', $agent)) {
-                $os = '<i class="ua-icon icon-win2"></i>Windows 10';
-            } else {
-                $os = '<i class="ua-icon icon-win2"></i>Windows X64';
-            }
-        } elseif (stripos($agent, 'android') !== false) {
-            if (preg_match('/android 9/i', $agent)) {
-                $os = '<i class="ua-icon icon-android"></i>Android Pie';
-            } elseif (preg_match('/android 8/i', $agent)) {
-                $os = '<i class="ua-icon icon-android"></i>Android Oreo';
-            } else {
-                $os = '<i class="ua-icon icon-android"></i>Android';
-            }
-        } elseif (stripos($agent, 'ubuntu') !== false) {
-            $os = '<i class="ua-icon icon-ubuntu"></i>Ubuntu';
-        } elseif (stripos($agent, 'linux') !== false) {
-            $os = '<i class= "ua-icon icon-linux"></i>Linux';
-        } elseif (stripos($agent, 'iPhone') !== false) {
-            $os = '<i class="ua-icon icon-apple"></i>iPhone';
-        } elseif (stripos($agent, 'mac') !== false) {
-            $os = '<i class="ua-icon icon-mac"></i>MacOS';
-        } elseif (stripos($agent, 'fusion') !== false) {
-            $os = '<i class="ua-icon icon-android"></i>Android';
-        } else {
-            $os = '<i class="ua-icon icon-linux"></i>Linux';
+        // mac
+        if ($parse->isOs('OS X')) {
+            $echo = '<i class="ua-icon icon-mac"></i>MacOS ' . $parse->os->version->toString();
         }
-        return $os;
+
+        // win
+        if ($parse->isOs('Windows')) {
+            if ((int) $parse->os->version->toString() ?: 1 > 7) {
+                $echo = '<i class="ua-icon icon-win2"></i>Windows ' . $parse->os->version->toString();
+            } else {
+                $echo = '<i class="ua-icon icon-win1"></i>Windows ' . $parse->os->version->toString();
+            }
+        }
+
+        // ubuntu
+        if ($parse->isOs('Ubuntu')) {
+            $echo = '<i class="ua-icon icon-ubuntu"></i>Ubuntu';
+        }
+
+        //debian
+        if ($parse->isOs('Debian')) {
+            $echo = '<i class="ua-icon icon-debian"></i>Debian';
+        }
+
+        //linux
+        if ($parse->isOs('Linux')) {
+            $echo = '<i class="ua-icon icon-linux"></i>Linux';
+        }
+
+        //android
+        if ($parse->isOs('Android')) {
+            $echo = '<i class="ua-icon icon-android"></i>Android ' . $parse->os->version->toString();
+        }
+
+        //ios || iphone
+        if ($parse->isOs('iOS') || $parse->isOs('iPhone')) {
+            $echo = '<i class="ua-icon icon-apple"></i>iOS ' . $parse->os->version->toString();
+        }
+
+        if ($echo === null) {
+            if ($parse->isType('desktop')) {
+                $echo = '<i class="ua-icon icon-desktop"></i>' . $parse->os->toString();
+            } elseif ($parse->isMobile()) {
+                $echo = '<i class="ua-icon icon-mobile"></i>' . $parse->os->toString();
+            } else {
+                $echo = $parse->os->toString();
+            }
+        }
+        return $echo ?: $parse->toString();
     }
 
     public static function cutIp(string $ip, int $length = 3, string $replace = '*')
