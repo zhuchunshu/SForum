954808264cf6a4a54f79f9aee815cb81b1142441
zhuchunshu <laravel@88.com>
2023-02-14 19:43:12 +0800

    优化:InvitationCode短代码渲染视图优化

--- app/Themes/CodeFec/resources/views/ShortCode/InvitationCode.blade.php
+++ app/Themes/CodeFec/resources/views/ShortCode/InvitationCode.blade.php
@@ -1,5 +1,5 @@
 <div class="row justify-content-center">
-    <div class="col-10">
+    <div class="col-12">
         <div class="card">
             <div class="card-header">
                 <h3 class="card-title">邀请码</h3>
