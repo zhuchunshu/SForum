65fc1d7f18bf8d965ce5de01d91397b3b5000663
zhuchunshu <laravel@88.com>
2023-02-20 22:58:51 +0800

    更新:优化topic-tag短代码渲染效果

--- app/Plugins/Topic/resources/views/ShortCode/tag.blade.php
+++ app/Plugins/Topic/resources/views/ShortCode/tag.blade.php
@@ -1,19 +1,16 @@
 <div class="row row-cards justify-content-center">
-    <div class="col-md-6">
-        <div class="card">
-            <div class="card-status-top" style="{{ Core_Ui()->Css()->bg_color($value->color) }}"></div>
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-auto">
-                        <span class="avatar" style="background-image: url({{ $value->icon }})"></span>
-                    </div>
-                    <div class="col">
-                        <a href="/tags/{{ $value->id }}.html"
-                           class="card-title text-h2">{{ $value->name }}</a>
-                        {{ \Hyperf\Utils\Str::limit(core_default($value->description, __("app.no description")), 32) }}
-                    </div>
+    <div class="col-md-10">
+        <a href="/tags/{{$value->id}}.html" class="card card-link text-primary-fg" style="background-color: {{$value->color}}!important;">
+            <div class="card-stamp">
+                <div class="card-stamp-icon bg-yellow">
+                    <!-- Download SVG icon from http://tabler-icons.io/i/star -->
+                    {!! $value->icon !!}
                 </div>
             </div>
-        </div>
+            <div class="card-body">
+                <h3 class="card-title">{{$value->name}}</h3>
+                <p>{{ \Hyperf\Utils\Str::limit(core_default($value->description, __("app.no description")), 32) }}</p>
+            </div>
+        </a>
     </div>
 </div>
