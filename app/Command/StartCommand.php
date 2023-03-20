38b3ba984534b37e01f70d2064a16e8cbc592bf6
zhuchunshu <laravel@88.com>
2023-03-20 20:30:23 +0800

    更新:启用热重载功能,优化启动速度

--- app/Command/StartCommand.php
+++ app/Command/StartCommand.php
@@ -12,11 +12,20 @@ namespace App\Command;
 
 use App\CodeFec\Install;
 use Hyperf\Command\Annotation\Command;
-use Hyperf\Command\Command as HyperfCommand;
+use Symfony\Component\Console\Command\Command as HyperfCommand;
+use Hyperf\Contract\ConfigInterface;
+use Hyperf\Contract\StdoutLoggerInterface;
+use Hyperf\Engine\Coroutine;
+use Hyperf\Server\ServerFactory;
 use Hyperf\Watcher\Option;
 use Hyperf\Watcher\Watcher;
 use Psr\Container\ContainerInterface;
+use Psr\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use InvalidArgumentException;
+
 
 /**
  * @Command
@@ -36,31 +45,70 @@ class StartCommand extends HyperfCommand
         $this->addOption('no-restart', 'N', InputOption::VALUE_NONE, 'Whether no need to restart server');
     }
 
-    public function handle()
+    protected function execute(InputInterface $input, OutputInterface $output)
     {
-        if (file_exists(BASE_PATH . '/app/CodeFec/storage/install.lock') || (new Install($this->output, $this))->getStep() >= 5) {
-            go(function () {
-                system_clear_cache();
-            });
-            $option = make(Option::class, [
-                'dir' => $this->input->getOption('dir'),
-                'file' => $this->input->getOption('file'),
-                'restart' => ! $this->input->getOption('no-restart'),
-            ]);
+        if (file_exists(BASE_PATH . '/app/CodeFec/storage/install.lock') || (new Install($output, $this))->getStep() >= 5) {
+            $this->checkEnvironment($output);
 
-            $watcher = make(Watcher::class, [
-                'option' => $option,
-                'output' => $this->output,
-            ]);
+            $serverFactory = $this->container->get(ServerFactory::class)
+                ->setEventDispatcher($this->container->get(EventDispatcherInterface::class))
+                ->setLogger($this->container->get(StdoutLoggerInterface::class));
 
-            $watcher->run();
+            $serverConfig = $this->container->get(ConfigInterface::class)->get('server', []);
+            if (! $serverConfig) {
+                throw new InvalidArgumentException('At least one server should be defined.');
+            }
+
+            $serverFactory->configure($serverConfig);
+
+            Coroutine::set(['hook_flags' => swoole_hook_flags()]);
+
+            $serverFactory->start();
         } else {
             $install = make(Install::class, [
-                'output' => $this->output,
+                'output' => $output,
                 'command' => $this,
             ]);
 
             $install->run();
+        }
+
+        return 0;
+    }
+
+    private function checkEnvironment(OutputInterface $output)
+    {
+        if (! extension_loaded('swoole')) {
+            return;
+        }
+        /**
+         * swoole.use_shortname = true       => string(1) "1"     => enabled
+         * swoole.use_shortname = "true"     => string(1) "1"     => enabled
+         * swoole.use_shortname = on         => string(1) "1"     => enabled
+         * swoole.use_shortname = On         => string(1) "1"     => enabled
+         * swoole.use_shortname = "On"       => string(2) "On"    => enabled
+         * swoole.use_shortname = "on"       => string(2) "on"    => enabled
+         * swoole.use_shortname = 1          => string(1) "1"     => enabled
+         * swoole.use_shortname = "1"        => string(1) "1"     => enabled
+         * swoole.use_shortname = 2          => string(1) "1"     => enabled
+         * swoole.use_shortname = false      => string(0) ""      => disabled
+         * swoole.use_shortname = "false"    => string(5) "false" => disabled
+         * swoole.use_shortname = off        => string(0) ""      => disabled
+         * swoole.use_shortname = Off        => string(0) ""      => disabled
+         * swoole.use_shortname = "off"      => string(3) "off"   => disabled
+         * swoole.use_shortname = "Off"      => string(3) "Off"   => disabled
+         * swoole.use_shortname = 0          => string(1) "0"     => disabled
+         * swoole.use_shortname = "0"        => string(1) "0"     => disabled
+         * swoole.use_shortname = 00         => string(2) "00"    => disabled
+         * swoole.use_shortname = "00"       => string(2) "00"    => disabled
+         * swoole.use_shortname = ""         => string(0) ""      => disabled
+         * swoole.use_shortname = " "        => string(1) " "     => disabled.
+         */
+        $useShortname = ini_get_all('swoole')['swoole.use_shortname']['local_value'];
+        $useShortname = strtolower(trim(str_replace('0', '', $useShortname)));
+        if (! in_array($useShortname, ['', 'off', 'false'], true)) {
+            $output->writeln("<error>ERROR</error> Swoole short function names must be disabled before the server starts, please set swoole.use_shortname='Off' in your php.ini.");
+            exit(SIGTERM);
         }
     }
 }
