931ca9d247d101ef13e0c372088d58cf622b523d
zhuchunshu <zhuchunshu999@gmail.com>
2023-07-28 23:30:46 +0800

    Fix:修复无法把艾特的用户转为链接的bug

--- app/Plugins/Core/src/Lib/TextParsing.php
+++ app/Plugins/Core/src/Lib/TextParsing.php
@@ -1,5 +1,13 @@
 <?php
 
+declare(strict_types=1);
+/**
+ * This file is part of zhuchunshu.
+ * @link     https://github.com/zhuchunshu
+ * @document https://github.com/zhuchunshu/SForum
+ * @contact  laravel@88.com
+ * @license  https://github.com/zhuchunshu/SForum/blob/master/LICENSE
+ */
 namespace App\Plugins\Core\src\Lib;
 
 use App\Plugins\User\src\Models\User;
@@ -7,24 +15,23 @@ use Hyperf\Utils\Str;
 
 class TextParsing
 {
-    public function keywords($keywords):string{
+    public function keywords($keywords): string
+    {
         return <<<HTML
 <a href="/keywords/{$keywords}.html">#{$keywords}</a>
 HTML;
-
     }
 
     public function at($username): string
     {
-        $username = Str::after($username,"@");
-		$username = str_replace(" ","",$username);
-		$username = urldecode($username);
-        if(User::query()->where("username",$username)->exists()) {
+        $username = Str::after($username, '@');
+        $username = trim($username);
+        $username = strip_tags($username);
+        if (User::query()->where('username', $username)->exists()) {
             return <<<HTML
 <a href="/users/{$username}.username">@{$username}</a>
 HTML;
         }
-
-        return "@".$username;
+        return '@' . $username;
     }
 }
