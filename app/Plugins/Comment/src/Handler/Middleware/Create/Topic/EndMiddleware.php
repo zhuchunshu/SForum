c928314e4d0b76904722003a00e46a2cd33360bf
zhuchunshu <zhuchunshu999@gmail.com>
2023-08-01 11:10:12 +0800

    Fix:修复不能发布评论的Bug

--- app/Plugins/Comment/src/Handler/Middleware/Create/Topic/EndMiddleware.php
+++ app/Plugins/Comment/src/Handler/Middleware/Create/Topic/EndMiddleware.php
@@ -1,2 +1,25 @@
 <?php
 
+declare(strict_types=1);
+/**
+ * This file is part of zhuchunshu.
+ * @link     https://github.com/zhuchunshu
+ * @document https://github.com/zhuchunshu/SForum
+ * @contact  laravel@88.com
+ * @license  https://github.com/zhuchunshu/SForum/blob/master/LICENSE
+ */
+namespace App\Plugins\Comment\src\Handler\Middleware\Create\Topic;
+
+use App\Plugins\Topic\src\Handler\Topic\Middleware\MiddlewareInterface;
+
+class EndMiddleware implements MiddlewareInterface
+{
+    public function handler($data, \Closure $next)
+    {
+        // 评论发表成功
+        // 添加发表成功事件
+        EventDispatcher()->dispatch(new \App\Plugins\User\src\Event\Task\Daily\CreateTopicComment($data['comment']['id']));
+
+        return redirect()->url('/' . $data['topic_id'] . '.html/' . $data['comment']['id'] . '?page=' . get_topic_comment_page($data['comment']['id']) . '&clean_topic_comment_content_cache=create_topic_comment_' . $data['topic_id'])->with('success', '发表成功!')->go();
+    }
+}
