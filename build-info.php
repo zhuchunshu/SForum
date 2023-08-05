4241cc92536c2f5cfc30f4fc6b5915b1c92cb69b
zhuchunshu <zhuchunshu999@gmail.com>
2023-08-05 20:38:06 +0800

    Publish:v2.5.9

--- build-info.php
+++ build-info.php
@@ -9,7 +9,7 @@ declare(strict_types=1);
  * @license  https://github.com/zhuchunshu/SForum/blob/master/LICENSE
  */
 return [
-    'version' => 'v2.5.8',
+    'version' => 'v2.5.9',
     'author' => 'zhuchunshu',
     'link' => 'https://www.runpod.cn',
 ];
