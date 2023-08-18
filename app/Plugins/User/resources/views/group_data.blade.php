8b8e2d8356be631d95cfc80ed48f92762c9545af
zhuchunshu <zhuchunshu999@gmail.com>
2023-08-19 02:38:53 +0800

    更新:优化用户组显示

--- app/Plugins/User/resources/views/group_data.blade.php
+++ app/Plugins/User/resources/views/group_data.blade.php
@@ -9,13 +9,24 @@
 
     <div class="row justify-content-center row-cards">
 
-        <div class="col-md-12">
-            <div class="border-0 card">
-                <div class="card-status-top" style="{{ Core_Ui()->Css()->bg_color($data->color) }}"></div>
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col-auto">
-                            <span class="avatar bg-azure-lt">
+
+        @if($userCount)
+            <div class="col-md-12">
+                <div style="margin-bottom: 10px">
+                    @if($user->count())
+                        <div class="card">
+                            <div class="card-header">
+                                <ol class="breadcrumb breadcrumb-arrows" aria-label="breadcrumbs">
+                                    <li class="breadcrumb-item"><a href="/">首页</a></li>
+                                    <li class="breadcrumb-item"><a href="/users">用户列表</a></li>
+                                    <li class="breadcrumb-item active" aria-current="page"><a href="#">用户组</a></li>
+                                    <li class="breadcrumb-item active" aria-current="page"><a href="#">{{$data->name}}</a></li>
+                                </ol>
+
+                            </div>
+                            <div class="card-header">
+                                <div class="row">
+                                    <div class="col-auto"><span class="avatar bg-azure-lt">
                                 {!! $data->icon?:'<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-users" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
    <path d="M9 7m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0"></path>
@@ -24,41 +35,32 @@
    <path d="M21 21v-2a4 4 0 0 0 -3 -3.85"></path>
 </svg>' !!}
                             </span>
-                        </div>
-                        <div class="col">
-                            <div class="row"><div class="col-auto"><h2>{{$data->name}}</h2></div><div class="col text-red">
-                                {{__("app.permission value")}}:{{$data['permission-value']}}</div></div>
-                            {{__("created at",['time' => $data->created_at])}} @if($data->updated_at), {{__("app.last update time",['time' => $data->updated_at])}}@endif
-                            @if($userCount),{{__("user.members in total",['total' => $userCount])}}@endif
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        @if($userCount)
-            <div class="col-md-12">
-                <div style="margin-bottom: 10px">
-                    @if($user->count())
-                        <div class="border-0 card">
-                            <div class="card-status-top" style="{{ Core_Ui()->Css()->bg_color($data->color) }}"></div>
-                            <div class="card-body">
-                                <h3 class="card-title">{{__("user.List of members under this user group")}}</h3>
-                                <div class="row">
-                                    @foreach($user as $value)
-                                    <div class="col-md-6" style="margin-bottom:5px">
+                                        </div>
+                                    <div class="col"><h3 class="card-title">{{$data->name}}</h3>
+                                    <span class="text-red">{{__("app.permission value")}}:{{$data['permission-value']}}</span></div>
+                                </div>
+                                <div class="card-actions">
+                                    @if($userCount)
+                                        ,{{__("user.members in total",['total' => $userCount])}}
+                                    @endif
+                                </div>
+                            </div>
+                            <div class="card-body row row-cards">
+                                @foreach($user as $value)
+                                    <a href="/users/{{$value->id}}.html" class="col-md-5 m-1 card border-1 card-body"
+                                       style="margin-bottom:5px;">
                                         <div class="row">
                                             <div class="col-auto">
-                                                <span class="avatar" style="background-image: url({{super_avatar($value)}})"></span>
+                                                <span class="avatar avatar-md"
+                                                      style="background-image: url({{super_avatar($value)}})"></span>
                                             </div>
                                             <div class="col">
-                                                <a href="/users/{{$value->id}}.html" class="text-body d-block text-truncate"><b>{{$value->username}}</b></a>
+                                                <span class="text-body h3 d-block text-truncate"><b>{{$value->username}}</b></span>
                                                 {{__("user.registration date")}}:{{$value->created_at}}
                                             </div>
                                         </div>
-                                    </div>
-                                    @endforeach
-                                </div>
+                                    </a>
+                                @endforeach
                             </div>
                         </div>
                     @else
@@ -72,6 +74,5 @@
         @endif
 
     </div>
-
 
 @endsection
