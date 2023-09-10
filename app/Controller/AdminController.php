3e8a9b698bed43a8bcaeaa4513fe7cc9d3f9ac63
朱纯树 <zhuchunshu999@gmail.com>
2023-09-10 12:52:55 +0800

    fix:修复后台登录强制检查验证码参数的问题

--- app/Controller/AdminController.php
+++ app/Controller/AdminController.php
@@ -40,7 +40,10 @@ class AdminController
         $username = $request->input('username');
         $password = $request->input('password');
         $captcha = $request->input('captcha');
-        if (! $username || ! $password || ! $captcha) {
+        if (! $username || ! $password) {
+            return Json_Api(403, false, ['请求参数不足!']);
+        }
+        if(get_options('admin_login_captcha_off')!=='true' && ! $captcha ){
             return Json_Api(403, false, ['请求参数不足!']);
         }
         if(get_options('admin_login_captcha_off')!=='true' && ! captcha()->check($captcha)){
