a128be88d8121b901972d1e2840cbbc14fd9bc64
zhuchunshu <zhuchunshu999@gmail.com>
2023-12-11 22:12:23 +0800

    fix:修复后台提示点击无反应的bug

--- resources/views/admin/hook/set_app_name.blade.php
+++ resources/views/admin/hook/set_app_name.blade.php
@@ -1,7 +1,7 @@
 @if(!get_options('APP_NAME') || !get_options('web_name'))
     <div class="my-2">
         <div class="alert alert-danger m-0">
-            请先<a href="#" class="alert-link">--->设置(点我)<---</a>网站名和应用名、否则会导致部分功能无法正常运行，例如：邮箱发信
+            请先<a href="/admin/setting" class="alert-link">--->设置(点我)<---</a>网站名和应用名、否则会导致部分功能无法正常运行，例如：邮箱发信
         </div>
     </div>
 
