12c0ef995e11391ac54af0e768b4884b549b36e1
Inkedus <zhuchunshu999@gmail.com>
2026-07-21 07:45:07 +0800

    chore(themes): remove unused default theme Nuxt layer tree
    Host no longer extends theme layers; runtime forbids SFORUM_THEME_LAYER.
    Delete the inert duplicate pages/components/CSS under layer/.

--- extensions/builtin/themes/sforum-default/layer/app/layouts/auth.vue
+++
@@ -1,4 +0,0 @@
-<template>
-  <!-- auth 页面不需要全局 navbar，直接渲染页面内容 -->
-  <slot />
-</template>
--- extensions/builtin/themes/sforum-default/layer/app/layouts/default.vue
+++
@@ -1,4 +0,0 @@
-<template>
-  <!-- 主题 layer 副本：公开 chrome 由 L1 模板岛或 Host fail-closed 壳拥有。 -->
-  <slot />
-</template>
